428627c7afd08962d4349b10948d9195dd15449e
jihun-noh <mon772@naver.com>
2020-08-13 03:20:00 +0900

    크롤러 수정

--- data/extract/crawler/crawling_encar.py
+++ data/extract/crawler/crawling_encar.py
@@ -1,22 +1,23 @@
 import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from pandas import DataFrame
 
-driver = webdriver.Chrome('C:/Users/Jihun/Desktop/chromedriver.exe')
-driver.get('http://www.encar.com/ev/ev_carsearchlist.do?carType=ev&searchType=model&TG.R=D#!%7B%22action%22%3A%22(And.Hidden.N._.CarType.A._.(C.GreenType.Y._.EvType.%EC%A0%84%EA%B8%B0%EC%B0%A8.)_.FuelType.%EC%A0%84%EA%B8%B0.)%22%2C%22toggle%22%3A%7B%7D%2C%22layer%22%3A%22%22%2C%22sort%22%3A%22ModifiedDate%22%2C%22page%22%3A1%2C%22limit%22%3A20%7D');   	# 구글에 접속
-time.sleep(1)
-
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('http://www.encar.com/ev/ev_carsearchlist.do?carType=ev&searchType=model&TG.R=D#!{"action":"(And.Hidden.N._.CarType.A._.(C.GreenType.Y._.EvType.전기차.)_.FuelType.전기.)","toggle":{},"layer":"","sort":"ModifiedDate","page":1,"limit":20}')
+driver.implicitly_wait(3)
 html = driver.page_source
-bs = BeautifulSoup(html, 'html.parser')
-manufacturer = bs.select('span.cls > strong')
-model = bs.select('span.cls > em')
-trim = bs.select('span.dtl > strong')
-year = bs.select('span.detail > span.yer')
-km = bs.select('span.detail > span.km')
-location = bs.select('span.detail > span.lo') + bs.select('span.detail > span.loc')
+driver.quit()
+
+soup = BeautifulSoup(html, 'html.parser')
+manufacturer = soup.select('span.cls > strong')
+model = soup.select('span.cls > em')
+trim = soup.select('span.dtl > strong')
+year = soup.select('span.detail > span.yer')
+km = soup.select('span.detail > span.km')
+location = soup.select('span.detail > span.lo') + soup.select('span.detail > span.loc')
 
 old_cars = []
-
 for car in zip(manufacturer, model, trim, year, km, location):
     old_cars.append(
         {
@@ -28,5 +29,7 @@ for car in zip(manufacturer, model, trim, year, km, location):
             'location' : car[5].text,
         }
     )
-print(old_cars)
-print(len(old_cars))
+
+data = DataFrame(old_cars)
+print(data)
+data.to_csv('../../csv/encar_{}.csv'.format(time.time()), mode='w')
