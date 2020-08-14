54ac5b706e3ab55c73ee284d6765a614bb5e6982
jihun-noh <mon772@naver.com>
2020-08-14 19:33:01 +0900

    연료별등록현황 csv화 처리

---
+++ data/extract/from_file/registration_status.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import pandas as pd
+sys.path.append('..')
+import settings
+
+def registration_status(filename):
+    excel = pd.ExcelFile(filename)
+    for sheet in excel.sheet_names:
+        if '연료별_등록현황' in sheet:
+            fuel_sheet = sheet
+    data = pd.read_excel(excel, sheet_name=fuel_sheet)
+    df = pd.DataFrame(data)
+    year_month = df.iat[0, 1].replace('.', '_')
+
+    df_gasoline = df.loc[3:19]
+    df_diesel = df.loc[20:36]
+    df_electric = df.loc[71:87]
+
+    df_gasoline.to_csv(os.path.join(settings.output_csv_dir, year_month + '_registration_gasoline.csv'), mode='w')
+    df_diesel.to_csv(os.path.join(settings.output_csv_dir, year_month + '_registration_diesel.csv'), mode='w')
+    df_electric.to_csv(os.path.join(settings.output_csv_dir, year_month + '_registration_electric.csv'), mode='w')
+
+registration_status(os.path.join(settings.input_xls_dir, '2020_07_car_registration.xlsx'))
