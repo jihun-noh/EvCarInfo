c839c9331bf6b63d1eee5e60970118a83299134a
jihun-hon <mon772@naver.com>
2020-08-18 02:59:38 +0900

    redis 모듈 수정

--- data/db/redis_module.py
+++ data/db/redis_module.py
@@ -1,11 +1,14 @@
 import redis
 
 class RedisModule():
-    def __init__(self, host, port, db):
-        self.conn = redis.Redis(host=host, port=port, db=db)
+    HOST = '54.180.99.44'
+    PORT = '6379'
+
+    def __init__(self):
+        self.r = redis.Redis(host=RedisModule.HOST, port=RedisModule.PORT, db=0)
 
     def set(self, key, value):
-        return self.conn.set(key, value)
+        return self.r.set(key, value)
 
     def get(self, key):
-        return self.conn.get(key)
+        return self.r.get(key)
