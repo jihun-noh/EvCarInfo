0f4201b25d15b0184a6a88098e6a4e6335276597
jihun-hon <mon772@naver.com>
2020-08-18 02:50:28 +0900

    크롤러 모듈 수정

--- data/extract/crawling/crawler/selenium_crawler.py
+++ data/extract/crawling/crawler/selenium_crawler.py
@@ -7,6 +7,8 @@ sys.path.append('..')
 import settings
 
 class SeleniumCrawler():
+    WAIT_TIME = 20
+
     def __init__(self, url, scrapping):
         self.webdriver = settings.webdriver
         self.url = url
@@ -15,13 +17,14 @@ class SeleniumCrawler():
     def set_crawler(self):
         driver = webdriver.Chrome(self.webdriver)
         driver.get(self.url)
-        driver.implicitly_wait(5)
+        driver.implicitly_wait(SeleniumCrawler.WAIT_TIME)
         self.html = driver.page_source
         driver.quit()
 
     def crawling(self):
         data = self.scrapping(self.html)
         self.dataframe = DataFrame(data)
+        return self.dataframe
 
     def save_to_csv(self, filename):
         save_file = os.path.join(settings.output_csv_dir, '{}_{}.csv'.format(filename, time.time()))
