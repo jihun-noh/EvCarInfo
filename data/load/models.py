2518eb89a7f3c48f5121f0a4f151978e0a5628aa
jihun-noh <mon772@naver.com>
2020-08-26 02:28:35 +0900

    임시파일

---
+++ data/load/models.py
@@ -0,0 +1,19 @@
+db table
+
+cars
+manufacturer	model	trim	mileage		year
+
+old_cars
+model	trim	year	km		location
+
+government_support
+model	trim	subsidy_gov		subsidy_local
+
+registration_status
+year_month	base	base_value	total
+			fuel	gasoline
+
+charging_station
+
+standard
+new		old
