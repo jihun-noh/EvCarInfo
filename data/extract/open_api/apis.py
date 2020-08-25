8ba3377372dda64f5e533593ae7a0b75c0971bbf
jihun-noh <mon772@naver.com>
2020-08-25 21:07:52 +0900

    api 변경

--- data/extract/open_api/apis.py
+++ data/extract/open_api/apis.py
@@ -1,19 +1,17 @@
 import sys
 import requests
-sys.path.append('..')
 import settings
 
 class DataGoKr():
     def __init__(self):
-        self.service_key = settings.data_go_kr_service_key
+        self.service_key = settings.DATA_GO_KR_SERVICE_KEY
 
-    def get_charging_station(self, page, row, addr=None):
+    def get_charging_station(self, page_no, page_size):
         params = {
             'serviceKey' : self.service_key,
-            'pageNo' : page,
-            'numOfRows' : row,
-            'addr' : addr,
+            'pageNo' : page_no,
+            'pageSize' : page_size,
         }
-        url = 'http://openapi.kepco.co.kr/service/EvInfoServiceV2/getEvSearchList'
+        url = 'http://open.ev.or.kr:8080/openapi/services/EvCharger/getChargerInfo'
         res = requests.get(url, params=params)
         return res
